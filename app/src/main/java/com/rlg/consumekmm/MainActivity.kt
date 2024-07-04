5d83ffec1ac21ed1c2cd6def5fd661633c401534
Rowan Gontier <rgontier@gmail.com>
2024-07-04 13:56:43 +1000

    Get shared UI to work.

--- app/src/main/java/com/rlg/consumekmm/MainActivity.kt
+++ app/src/main/java/com/rlg/consumekmm/MainActivity.kt
@@ -7,17 +7,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rlg.consumekmm.ui.theme.ConsumeKMMTheme
 import com.rlg.play_kotlin_multi_plat.domain.viewmodels.SharedVMState
+import com.rlg.play_kotlin_multi_plat.getPlatform
+import com.rlg.play_kotlin_multi_plat.ui.SharedUI.FirstUI
 
 class MainActivity : ComponentActivity() {
     private val viewModel: AndroidSharedViewModel = AndroidSharedViewModel()
@@ -58,26 +63,44 @@ fun MainScreen(
     modifier: Modifier = Modifier
 ) {
     Column(modifier = modifier.padding(16.dp)) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = getPlatform().name, modifier = Modifier.padding(bottom = 24.dp), style = MaterialTheme.typography.headlineLarge)
+
+        }
         if (isLoading) {
             Text(text = "Loading...")
         } else if (error != null) {
             Text(text = "Error: ${error.message}")
         } else {
-            Text(text = "Data: $data")
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Data: $data")
+                Button(onClick = onFetchData) {
+                    Text(text = "Fetch Data")
+                }
+            }
         }
-        Text(text = "Counter: $counter")
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(onClick = onFetchData) {
-            Text(text = "Fetch Data")
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Counter: $counter")
+            Button(onClick = onIncrementCounter) {
+                Text(text = "Increment Counter")
+            }
         }
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(32.dp))
 
-        Button(onClick = onIncrementCounter) {
-            Text(text = "Increment Counter")
-        }
+        FirstUI()
     }
 }
 
